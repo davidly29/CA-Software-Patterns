bdb00dafd8278feb64a14b08b5a8bd064203f881
davidly29 <davidly255@gmail.com>
2020-04-14 22:10:36 +0100

    html changes to and add stock fixes

--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -3,32 +3,41 @@ package com.example.demo.controller;
 import com.example.demo.dao.Stock_Item;
 import com.example.demo.service.StockItemService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping(value = "/stock")
+@Controller
+@EnableWebMvc
 public class StockController {
     private StockItemService StockService;
 
     public StockController(StockItemService stockService) {
         StockService = stockService;
     }
-    @GetMapping
-    public ResponseEntity<List<Stock_Item>> findAll() {
-        return ResponseEntity.ok(StockService.findAll());
+    @RequestMapping("/addStockItem")
+    public String welcome() {
+        return "addStock";
     }
-    @PostMapping()
-    public Stock_Item create(@RequestBody Stock_Item acc) {
-        return StockService.save(acc);
+    @GetMapping("/stock")
+    public String getAllStockItems(Model model) {
+        model.addAttribute("stocks", StockService.findAll());
+        return "stock";
     }
-    @GetMapping("/{id}")
+    @PostMapping("/addStock")
+    public String create(@ModelAttribute("stock_item")Stock_Item acc) {
+        StockService.save(acc);
+        return "redirect:/test";
+    }
+    @GetMapping("/getStock/{id}")
     public Optional<Stock_Item> getStockById(@PathVariable long id) {
         return StockService.findById(id);
     }
-    @DeleteMapping("/remove/{id}")
+    @DeleteMapping("/removeStock/{id}")
     void deleteCustomer(@PathVariable Long id ) {
         StockService.deleteById(id);
     }
